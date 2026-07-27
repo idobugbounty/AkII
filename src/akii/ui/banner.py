8ac761d510d81bf30d1a9468c5185b60e2b0965c
MintTester <danghai31807@gmail.com>
2026-07-27 19:48:33 +0700

    Edit name

--- src/akii/ui/banner.py
+++ src/akii/ui/banner.py
@@ -34,7 +34,7 @@ def get_banner():
 #+#║    #+#║ #+#║  #+#║ #+#║ #+#║ v{__version__}
 ###║    ###║ ###║  ###║ ###║ ###║
 ═══╝    ═══╝ ═══╝  ═══╝ ═══╝ ═══╝ 
-        - MintTester-IO -
+        - Mintesio -
 """
 	
 	return akii
